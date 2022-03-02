6cc4809766c327bbfb01b463a8730ac3f2496c40
T Chan <timmeh.chan@gmail.com>
2022-03-01 23:36:54 -0800

    removed unnecessary files

---
+++ course_inventory_scrapper.py
@@ -0,0 +1,18 @@
+#! python3
+
+import requests
+from bd4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+
+
+# A scraper for the case western MS Data Science program webpage
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
